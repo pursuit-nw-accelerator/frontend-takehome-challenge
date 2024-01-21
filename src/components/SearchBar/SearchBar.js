aa34096b3be806f5d242073e60f032fe8e54f132
Mike Boyle <mikesboyle@gmail.com>
2024-01-21 12:09:33 -0500

    update tasks

--- src/components/SearchBar/SearchBar.js
+++
@@ -1,9 +0,0 @@
-import './SearchBar.css';
-
-const SearchBar = () => {
-  return (
-    <input type="text" placeholder="Search by name, country, or company" />
-  );
-};
-
-export default SearchBar;
