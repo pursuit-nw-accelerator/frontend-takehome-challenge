d401a24b476a0aed00d8d42c93bcdd290e66742c
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2024-03-04 10:30:06 -0500

    rebdered all the hobbies from users for filterpurpose

--- src/components/FilterBar/FilterBar.js
+++ src/components/FilterBar/FilterBar.js
@@ -1,7 +1,27 @@
 import './FilterBar.css';
+// import Hobby from'./Hobby'
 
-const FilterBar = () => {
-  return <div>TODO: Add your filter buttons here</div>;
+
+const FilterBar = ({users}) => {
+  // console.log(users[0].hobbies);
+  const hobbyArr =[];
+  users.filter(user => hobbyArr.push(...user.hobbies));
+  const hobby = hobbyArr.filter((hobby,i,arr) => arr.indexOf(hobby) === i )
+  // console.log(hobby);
+
+  const handleOnClick = () => {
+    console.log("clicked");
+  }
+  return( 
+    <>
+    <h2>Filter by hobby</h2>
+      <div>
+        <div className='hobby__container'>
+        {hobby.map((hobby, i) => <button key={i} onClick= {handleOnClick}  className='hobby'>{hobby}</button>)}
+        </div>
+      </div>
+    </>
+    )
 };
 
 export default FilterBar;
