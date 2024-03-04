2f2bb5587f360160f67ea65bae5fc728ee4d4404
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2024-03-04 10:28:08 -0500

    fetched data from api and passed it to users comp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import FilterBar from './components/FilterBar/FilterBar';
 import Users from './components/Users/Users';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  const [ users, setUsers ] = useState([]);
+  const [loading, setLoading ] = useState(false)
   // TODO: Fetch data here
+  const fetchData = async () =>  {
+    try {
+      setLoading(true)
+      const response = await fetch('https://users-app-backend.onrender.com/users');
+      const { data, err: errMessage } = await response.json();
+      if(response.ok){
+        setUsers(data);
+        console.log(users)
+      }else {
+        throw new Error({error: errMessage})
+      }
+    }catch(err) {
+      console.err(err)
+    }finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  },[]);
 
   return (
     <div className="App">
       <h1>Our Users</h1>
-      <FilterBar />
-      <Users />
+      {loading ? "..." : (
+        <>
+          <FilterBar users={users}/>
+          <Users users={users}/>
+       </> 
+      )}
+      
     </div>
   );
 }
