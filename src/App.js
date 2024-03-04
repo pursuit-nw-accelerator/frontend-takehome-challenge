470dcc885bb3fe837fee321e276ad4776c5e1ff5
gitlep1 <privserv4@gmail.com>
2024-03-04 10:09:53 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,17 +1,56 @@
-import FilterBar from './components/FilterBar/FilterBar';
-import Users from './components/Users/Users';
-import './App.css';
+import "./App.scss";
+import { useEffect, useState } from "react";
+import FilterBar from "./components/FilterBar/FilterBar";
+import Users from "./components/Users/Users";
 
-function App() {
-  // TODO: Fetch data here
+const API = process.env.REACT_APP_API_URL;
 
-  return (
-    <div className="App">
-      <h1>Our Users</h1>
-      <FilterBar />
-      <Users />
-    </div>
-  );
-}
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      setError("");
+      setLoading(true);
+
+      await fetch(API)
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error("Something went wrong");
+          }
+          return res.json();
+        })
+        .then(({ data }) => setUsers(data))
+        .catch((err) => setError(err.message))
+        .finally(() => setLoading(false));
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  const renderData = () => {
+    if (error) {
+      return <div>ERROR: {error}</div>;
+    } else if (loading) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <>
+          <h1>Our Users</h1>
+          <FilterBar />
+          <Users users={users} />
+        </>
+      );
+    }
+  };
+
+  return <div className="App">{renderData()}</div>;
+};
 
 export default App;
