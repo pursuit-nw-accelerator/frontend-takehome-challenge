937fda9f7033a815d32dc906ba7d8ac8804b9159
Wwright91 <wisdomwright-martin@pursuit.org>
2024-03-05 16:33:01 -0500

    Fetched data from the API 1. Made an async fetch to the backend url using useeffect 2. Created state for the users data 3. Added .env to .gitignore file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import FilterBar from './components/FilterBar/FilterBar';
-import Users from './components/Users/Users';
-import './App.css';
+import FilterBar from "./components/FilterBar/FilterBar";
+import Users from "./components/Users/Users";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 function App() {
-  // TODO: Fetch data here
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch(`${API_URL}/users`);
+        const json = await res.json();
+
+        if (res.ok) {
+          setUsers(json.data)
+        } else {
+          throw new Error(json.error);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
