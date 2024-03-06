3627e4a3f70c15a412070173f098c979ada57055
crsvalle <cristian.valle@pursuit.org>
2024-03-06 07:59:08 -0500

    fetched users w/ error state + loading

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
 import FilterBar from './components/FilterBar/FilterBar';
 import Users from './components/Users/Users';
+import { useEffect, useState } from "react";
+
 import './App.css';
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 function App() {
   // TODO: Fetch data here
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const fetchData = async () => {
+    try {
+      setLoading(true)
+      setError('');
+      const response = await fetch(`${API_URL}users`);
+      const { data, error: errorMsg } = await response.json();
+      if (response.ok) {
+        setUsers(data)
+      } else {
+        throw new Error(errorMsg);
+      }
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false)
+    }
+  }
 
-  return (
-    <div className="App">
-      <h1>Our Users</h1>
-      <FilterBar />
-      <Users />
-    </div>
-  );
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const renderContent = () => {
+    if (loading) {
+      return <div className="Loading">Loading...</div>;
+    } else if (error) {
+      return <div className="Error">Error: {error} </div>;
+    } else {
+      return (<div className="App">
+        <h1>Our Users</h1>
+        <FilterBar />
+        <Users users={users} />
+      </div>);
+    }
+  };
+  return <div className="App">{renderContent()}</div>;
+
+
 }
 
 export default App;
