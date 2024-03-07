37f216bae6eb1388d8f65e0f4327e585c69cad0e
crsvalle <cristian.valle@pursuit.org>
2024-03-06 23:35:02 -0500

    added hobby buttons

--- src/components/FilterBar/FilterBar.js
+++ src/components/FilterBar/FilterBar.js
@@ -8,13 +8,34 @@ const FilterBar = ({users, setExpanded}) => {
   const toggleCollapseAll = () =>{
     setExpanded([])
   }
+  const listOfHobbies = [];
 
+  users.forEach(user => {
+    user.hobbies.forEach((hobby) => {
+      if (!listOfHobbies.includes(hobby)){
+        listOfHobbies.push(hobby)
+      }
+    })
+  });
+
+  listOfHobbies.sort((a,b) => {
+    if(a < b){
+      return -1
+    }
+    else if (b < a){
+      return 1
+    }
+    else{return 0}
+  })
+  console.log(listOfHobbies)
   return(
-  <div>
-    <div>
-      s
+  <div className='filterBar'>
+    <div className='hobbyList'>
+      {listOfHobbies.map((hobby) => {
+        return <button>{hobby}</button>
+      })}
     </div>
-    <div>
+    <div className='expandCollapseAll'>
       <button onClick={toggleAll}>Expand All</button>
       <button onClick={toggleCollapseAll}>Collapse All</button>
     </div>
