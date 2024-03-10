51afd0b35d4b443143d4714d5e23dfb84bb1a7ea
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2024-03-09 21:36:47 -0500

    made state for expand and collapse, selected category and passed it down as prop to child component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import './App.css';
 import { useState } from 'react';
 import { useEffect } from 'react';
 
+const API = process.env.REACT_API_URL
+
 function App() {
   const [ users, setUsers ] = useState([]);
-  const [loading, setLoading ] = useState(false)
+  const [loading, setLoading ] = useState(false);
+  const [error, setError ] = useState("");
+  //showmore and less liftup state from user comp
   const [ toggle, setToggle ] = useState([]);
+  const [ selectedHobby, setSelectedHobby ] = useState([]);
 
+  const arr = [];
+  users.forEach(user => arr.push(...user.hobbies));
+  const hobbyList = arr.filter((el, i, arr) => arr.indexOf(el) === i).sort()
 
   const handleToggle = (id) => {
     if(toggle.includes(id)){
@@ -18,52 +26,83 @@ function App() {
     }
   }
 
-  // TODO: Fetch data here
-  const fetchData = async () =>  {
-    try {
-      setLoading(true)
-      const response = await fetch('https://users-app-backend.onrender.com/users');
-      const { data, err: errMessage } = await response.json();
-      if(response.ok){
-        setUsers(data);
-         console.log(users)
-      }else {
-        throw new Error({error: errMessage})
-      }
-    }catch(err) {
-      console.err(err)
-    }finally {
-      setLoading(false)
-    }
+  const handleByExpand = () => {
+    setToggle(users.map(user => user.id))
+  }
+  
+  const handleByCollapse = () => {
+    setToggle([])
   }
 
-  useEffect(() => {
-    fetchData()
-  },[]);
+  const handleFilterBtnClick = (hobby) => {
+    let category = hobby.target.value
+    if(selectedHobby.includes(category)){
+      setSelectedHobby(selectedHobby.filter(currHobby => currHobby !== category))
+    }else {
+      setSelectedHobby([...selectedHobby, category])
+    }
+  }
+  
+  const filterByHobby = () => {
+  if(selectedHobby.length > 0){
+    let filterUsers = users.filter(user => user.hobbies.some(hobby => selectedHobby.includes(hobby)))
+    setUsers(filterUsers)
+  }
+}
 
-  const filterUser = (filter) => {
-   const { hobbies } = users[0]
-   console.log(users[0].hobbies)
-     if(filter.length > 0){
-      console.log(filter);
-      const filteredUser = filter.filter(hobby => hobbies.includes(hobby));
-       setUsers(filteredUser)
-     }
+// TODO: Fetch data here
+const fetchData = async () =>  {
+  try {
+    setLoading(true);
+    setError("");
+    const response = await fetch('https://users-app-backend.onrender.com/users');
+    const { data, error: errMessage } = await response.json();
+    if(response.ok){
+      setUsers(data);
+    }else {
+      throw new Error(errMessage)
+    }
+  }catch(err) {
+    setError(err.message)
+  }finally {
+    setLoading(false)
+  }
+}
+
+useEffect(() => {
+  fetchData()
+},[]);
+
+useEffect(() => {
+  filterByHobby()
+}, [selectedHobby])
+
+  const renderContent = () => {
+    if(loading) {
+      return <div>Loading...</div>
+    }else if(error){
+      return <div>Error: {error}</div>
+    }else {
+     return( <>
+          <FilterBar 
+          hobbyList={hobbyList} 
+          handleByCollapse={handleByCollapse} 
+          handleByExpand={handleByExpand} 
+          selectedHobby={selectedHobby}
+          handleFilterBtnClick={handleFilterBtnClick}
+          />
+          <Users 
+          users={users}
+          toggle={toggle}
+          onClick={handleToggle} />
+       </> )
+    }
   }
 
   return (
     <div className="App">
       <h1>Our Users</h1>
-      {loading ? "Loading..." : (
-        <>
-          <FilterBar users={users} filterUser={filterUser}/>
-          <Users 
-          users={users}
-          toggle={toggle}
-          onClick={handleToggle} />
-       </> 
-      )}
-      
+      <div>{renderContent()}</div>
     </div>
   );
 }
