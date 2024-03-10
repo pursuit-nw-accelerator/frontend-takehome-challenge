7ad22aa502908940e7f56158d2f735679bffd2b2
Joanavel Pascual <joanavelpascual@pursuit.org>
2024-03-09 19:53:06 -0500

    added filter message back

--- src/components/FilterBar/FilterBar.js
+++ src/components/FilterBar/FilterBar.js
@@ -4,16 +4,25 @@ import "./FilterBar.css";
 const FilterBar = ({ users: originalUsers, setFilteredUsers, fetchUsers, expandAllUsers, collapseAllUsers }) => {
   const [selectedHobbies, setSelectedHobbies] = useState([]);
   const [filteredUsers, setFilteredUsersLocally] = useState(originalUsers);
+  const [noUsersMessage, setNoUsersMessage] = useState(null);
 
   useEffect(() => {
     if (selectedHobbies.length === 0) {
       setFilteredUsers(filteredUsers);
+      setNoUsersMessage(null);
     } else {
       const filtered = originalUsers.filter((user) =>
         selectedHobbies.every((hobby) => user.hobbies.includes(hobby))
       );
       setFilteredUsersLocally(filtered);
       setFilteredUsers(filtered); 
+
+      if (filtered.length === 0) {
+        const selectedFilters = selectedHobbies.join(", ");
+        setNoUsersMessage(`No users match the filters: ${selectedFilters}`);
+      } else {
+        setNoUsersMessage(null);
+      }
     }
   }, [selectedHobbies, originalUsers, filteredUsers, setFilteredUsers]);
 
@@ -35,28 +44,31 @@ const FilterBar = ({ users: originalUsers, setFilteredUsers, fetchUsers, expandA
   };
 
   return (
-<div className="FilterBar">
-  <div className="hobby-buttons">
-    {allHobbies.sort().map((hobby) => (
-      <button key={hobby} className={selectedHobbies.includes(hobby) ? "selected" : ""} onClick={() => toggleHobby(hobby)} >
-        {hobby}
-      </button>
-    ))}
-  </div>
-  <div className="action-buttons">
-    <button className="expand" onClick={expandAllUsers}>
-      Expand All
-    </button>
-    <button className="collapse" onClick={collapseAllUsers}>
-      Collapse All
-    </button>
-    <button className="clear" onClick={clearAllHobbies}>
-      Clear All
-    </button>
-  </div>
-</div>
-
-
+    <div className="FilterBar">
+      <div className="hobby-buttons">
+        {allHobbies.sort().map((hobby) => (
+          <button
+            key={hobby}
+            className={selectedHobbies.includes(hobby) ? "selected" : ""}
+            onClick={() => toggleHobby(hobby)}
+          >
+            {hobby}
+          </button>
+        ))}
+        {noUsersMessage && <div className="no-users">{noUsersMessage}</div>}
+      </div>
+      <div className="action-buttons">
+        <button className="expand" onClick={expandAllUsers}>
+          Expand All
+        </button>
+        <button className="collapse" onClick={collapseAllUsers}>
+          Collapse All
+        </button>
+        <button className="clear" onClick={clearAllHobbies}>
+          Clear All
+        </button>
+      </div>
+    </div>
   );
 };
 
