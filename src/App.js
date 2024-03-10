770dd0bec9209c66227036b93d10ef04b64e6199
Allahvel Salisbury <allahvelsalisbury@pursuit.org>
2024-03-10 06:50:36 -0400

    Implemented necessary useStates(users,loading,errors,selectedHobbies, hobbies). Functions to handle and filter selected hobbies have been made along with filtering users by selected hobbies. If no matches are found, the page will dispaly a message saying so along with a comma separated list of the filters that do not match. Data is fetched, healthy responses are made when successful and errors are caught, returning the error message. All content is rendered.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,85 @@
+//Dependencies
+import React , { useEffect , useState } from 'react';
+
+//Components
 import FilterBar from './components/FilterBar/FilterBar';
 import Users from './components/Users/Users';
 import './App.css';
 
 function App() {
-  // TODO: Fetch data here
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState("")
+  const [selectedHobbies, setSelectedHobbies] = useState([])
+  const [hobbies, setHobbies] = useState([])
+  const fetchData = async () => {
+    try {
+      setLoading(true)
+      setError("")
+      const response = await fetch("https://users-app-backend.onrender.com/users")
+      const { data , error: errorMsg } = await response.json();
+      if(response.ok) {
+        setUsers(data);
+        console.log(data)
+        
+        const allHobbies = data.reduce((hobbyList, user) => {
+          user.hobbies.forEach(hobby => {
+            if(!hobbyList.includes(hobby)){
+              hobbyList.push(hobby)
+            }
+          })
+          return hobbyList
+        }, [])
 
-  return (
-    <div className="App">
-      <h1>Our Users</h1>
-      <FilterBar />
-      <Users />
-    </div>
-  );
+        console.log(allHobbies)
+        setHobbies(allHobbies.sort())
+      } else {
+        throw new Error(errorMsg)
+      }
+    } catch(err) {
+      setError(err.message);
+    } finally {
+      setLoading(false)
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, [])
+  
+const handleHobbies = (hobby) => {
+  if(selectedHobbies.includes(hobby)) {
+    setSelectedHobbies(selectedHobbies.filter((selected) => selected !== hobby))
+  } else {
+    setSelectedHobbies([...selectedHobbies, hobby])
+  }
 }
 
+const renderContent = () => {
+ 
+         const filteredUsers = users.filter(user => {
+        return selectedHobbies.every(selectedHobby => user.hobbies.includes(selectedHobby)) 
+      });
+
+  const selectedFilters = selectedHobbies.join(', ')
+
+  if (loading) {
+      return <div className="Loading">One second...</div>;
+    } else if (error) {
+      return <div className="Error">Error: {error} </div>;
+    } else {
+      return (
+      <div>
+      <FilterBar hobbies={hobbies} selectedHobbies={selectedHobbies} onHobbyClick={handleHobbies}/>
+      {filteredUsers.length > 0 ? ( 
+        <Users users={filteredUsers} selectedHobbies={selectedHobbies}/> 
+      ) : ( 
+        <p>No users match the filters: {selectedFilters}</p>
+      )}
+      </div>
+      )
+    }
+  };
+  return <div className="App">{renderContent()}</div>;
+}
+  
 export default App;
