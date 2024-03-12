0372d08075346aa298ecd09591308cc1a3e7d42a
Allahvel Salisbury <allahvelsalisbury@pursuit.org>
2024-03-12 18:35:58 -0400

    Properly passing prop for expanded and toggleExpanded to Users.

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,45 +1,20 @@
-import { useState } from 'react';
 import User from '../User/User';
 import './Users.css';
 
-const Users = ({ users = [] }) => {
-  const [expanded, setExpanded] = useState([]);
+const Users = ({ users = [], expanded, toggleExpanded }) => {
 
-  const toggleExpanded = (id) => {
-    if (expanded.includes(id)) {
-      setExpanded(expanded.filter((currId) => currId !== id));
-    } else {
-      setExpanded([...expanded, id]);
-    }
-  };
-
-  const expandAll = () => {
-    const allUserIds = users.map((user) => user.id);
-    setExpanded(allUserIds);
-  };
-
-  const collapseAll = () => {
-    setExpanded([]);
-  };
-
-  return (
-    <article className="Users">
-      <button className="expand_button" onClick={expandAll}>
-        Expand All
-      </button>
-      <button className="collapse_button" onClick={collapseAll}>
-        Collapse All
-      </button>
-      {users.map((user) => (
-        <User
-          key={user.id}
-          user={user}
-          expanded={expanded.includes(user.id)}
-          onClick={() => toggleExpanded(user.id)}
-        />
-      ))}
-    </article>
-  );
+return (
+  <article className="Users">  
+    {users.map((user) => (
+      <User
+        key={user.id}
+        user={user}
+        expanded={expanded.includes(user.id)}
+        onClick={() => toggleExpanded(user.id)}
+      />
+    ))}
+  </article>
+);
 };
 
 export default Users;
